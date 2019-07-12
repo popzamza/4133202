5843b09200686f9183440fabeaeda9bdad8968de
popzamza <600112417039@bru.ac.th>
2019-07-12 15:41:37 +0700

    init project

---
+++ php/add.php
@@ -0,0 +1,22 @@
+<?php
+echo $_POST['user'];
+
+echo "Hello World !!!";
+
+$name = "Anuthida";
+echo "Miss." .$name;
+echo "<br>";
+
+echo sum(5,3);
+
+//function
+function sum($A,$B){
+    return $A + $B;
+}
+
+
+
+?>
+
+<!--short term, use one  line only.-->
+<?= 'My name is aom';
